ce51a968775b1cea2d61cd709507fa2767e308de
HeerMehta <mehtaheerh@gmail.com>
2023-09-03 11:58:07 +0530

    commit

--- lecture2.cpp
+++ lecture2.cpp
@@ -15,5 +15,14 @@ int main()
     // }
     // return 0;
 
-    
+    //Output question 1
+    int a = 2;
+    int b = a+1;
+
+    if((a=3) == b){
+        cout << a;
+    }
+    else{
+        cout << b;
+    }
 }
