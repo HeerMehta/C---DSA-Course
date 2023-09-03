f77f32817bf2e9dd752a3642b0d34f545dc09b50
HeerMehta <mehtaheerh@gmail.com>
2023-09-03 11:56:45 +0530

    first commit

---
+++ lecture2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //Check which number from a and b is greater
+    // int a = 2;
+    // int b = 3;
+    // if(a > b){
+    //     cout << "a is greater";
+    // }
+    // else{
+    //     cout << "b is greater or equal to a";
+    // }
+    // return 0;
+
+    
+}
